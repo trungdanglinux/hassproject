c06271fad555059f0ec6fa3258732df4507036e2
trungdanglinux <59747048+trungdanglinux@users.noreply.github.com>
2020-03-14 02:41:14 +0200

    Updating

--- homekitchen.py
+++ homekitchen.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+import sys
 import RPi.GPIO as GPIO
 import time
 import Adafruit_DHT
@@ -15,59 +15,58 @@ RELAY = 27
 
 LCD_LINE_1 = 0x80 # LCD RAM address for the 1st line
 LCD_LINE_2 = 0xC0 # LCD RAM address for the 2nd line
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(MOTION_SENSOR,GPIO.IN)
 GPIO.setup(SMOKE_SENSOR,GPIO.IN)
 GPIO.setup(FLAME_SENSOR,GPIO.IN)
-GPIO.setup(LED1,GPIO.OUT)
-GPIO.setup(RELAY,GPIO.IN)
+GPIO.setup(LED1,GPIO.OUT, initial = 0)
+GPIO.setup(RELAY,GPIO.OUT, initial =0)
 
-workingStove = GPIO.input(RELAY)
-smoke = GPIO.input(SMOKE_SENSOR)
-flame = GPIO.input(FLAME_SENSOR)
-motion = GPIO.input(MOTION_SENSOR)
-light = GPIO.output(LED1)
+
 humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, DHT11)
 
+high = GPIO.HIGH
+low = GPIO.LOW
+def tem():
+  if temperature >= 60: 
+      GPIO.output(RELAY,HIGH)
+      print("The stove is off due to leaving a stove on when not using!")
+      print("The temperature at the moment is %{0:0.1f}*C".format(temperature))
+
 def offStove():
-    while workingStove:
-        if motion == False:
-            if smoke == False or flame == True: #smoke is off when it's deteced 
-                GPIO.output(RELAY,GPIO.LOW)
+
+    while GPIO.input(RELAY) == high:
+        tem()
+        if GPIO.input(MOTION_SENSOR) == low:
+            if GPIO.input(SMOKE_SENSOR) == low or GPIO.input(FLAME_SENSOR) == low: #smoke sensor and flame are off when it's deteced 
+                GPIO.output(RELAY,low)
                 print("The stove is off due to burning")
         else:
-            if flame == True:
-                GPIO.output(RELAY,GPIO.LOW)
+            if GPIO.input(FLAME_SENSOR) == low:
+                GPIO.output(RELAY,low)
                 print("Stove is off due to flame!")
-        if temperature >= 50: 
-            GPIO.output(RELAY,GPIO.LOW)
-            print("The stove is off due to leaving a stove on when not using!")
-            print("The temperature at the moment is %{0:0,1f}*C".format(temperature))
     else:
-        if smoke == False or flame == True: #smoke is off when it's deteced 
-            GPIO.output(RELAY,GPIO.LOW)
-            print("Stove is off!")        
-
+        if GPIO.input(SMOKE_SENSOR) == low or GPIO.input(FLAME_SENSOR) == low:
+            GPIO.output(RELAY,low)
+            print("Stove is off!")
 def kitchenLight():
-    if motion == False and light == True :
-        time.sleep(90)
-        GPIO.output(LED1,GPIO.LOW)
-        print("The light is off!")
-    elif motion == True and light == False :
-        GPIO.output(LED1,GPIO.HIGH)
-        print("the light is on!")
+    while GPIO.input(MOTION_SENSOR) == low :
+         if GPIO.input(LED1) == high :
+            time.sleep(20)
+            print("The light is off!")
+            GPIO.output(LED1,low)
+    else:
+        if GPIO.input(MOTION_SENSOR) == high and GPIO.input(LED1) == low:
+            print("the light is on!")
+            GPIO.output(LED1,high)
 
 try:
-    offStove()
-    kitchenLight()
-    lcd_i2c.lcd_string('Temp={0:0.1f}*C'.format(temperature),LCD_LINE_1)
-    lcd_i2c.lcd_string('Humidity={0:0.1f}%'.format(humidity),LCD_LINE_2)
-except:
-    print("There is somethig wrong!")
-    
-        
-
-
-
-    
+   while True:  
+     offStove()
+     kitchenLight()
+  lcd_i2c.lcd_string('Temp={0:0.1f}*C'.format(temperature),LCD_LINE_1)
+  lcd_i2c.lcd_string('Humidity={0:0.1f}%'.format(humidity),LCD_LINE_2)
+except KeyboardInterrupt:
+  print("The program stopped!")
 
